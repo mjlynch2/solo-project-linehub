f4eba3a0bc53f3ba953d91a86a1dfacad9cc40cf
Mike Lynch <mjlynch2@gmail.com>
2019-10-19 10:49:42 -0500

    add post, delete, and put routes for adding, editing, and deleting stations on the server

--- server/routes/station.router.js
+++ server/routes/station.router.js
@@ -19,7 +19,21 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
-
+    const query = `INSERT INTO "station" ("station_name") VALUES ($1);`;
+    console.log(req.body);
+    res.sendStatus(201); 
 });
+
+router.delete('/:id', (req, res) => {
+    const query = `DELETE FROM "station" WHERE "id" = $1;`;
+    console.log(req.params.id);
+    res.sendStatus(200);
+})
+
+router.put('/:id', (req, res) => {
+    const query = `UPDATE "station SET "station_name" = $1 WHERE "id" = $2;`;
+    console.log(req.params.id, req.body);
+    res.sendStatus(200);
+})
 
 module.exports = router;
